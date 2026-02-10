cebc1ac595945ae6e4c70c74cd392c2cc9c28eb5
Bookfow <junepk@bjpublic.co.kr>
2026-02-10 19:33:24 +0900

    Fix useState import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAuth } from '@/lib/auth-context'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
