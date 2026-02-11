e96a963ccfbe212fa2fe76f93ff24e2121ff26de
Bookfow <junepk@bjpublic.co.kr>
2026-02-12 02:05:04 +0900

    Enable sidebar toggle with hamburger menu on all screen sizes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -242,7 +242,7 @@ export function Sidebar({ isOpen, onClose }: SidebarProps) {
         className={`
           fixed top-0 left-0 h-screen bg-white border-r z-40 transition-all lg:static
           ${isOpen ? 'translate-x-0 w-60 lg:w-60' : '-translate-x-full lg:translate-x-0 lg:w-16'}
-xl:w-60
+          xl:w-60
         `}
       >
         <SidebarContent />
