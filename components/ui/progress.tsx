a19a3e7f5e00bade19104fcfe7226904bca1aa98
Bookfow <junepk@bjpublic.co.kr>
2026-02-11 23:29:58 +0900

    Simplify Progress component

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -4,10 +4,14 @@ import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
 import { cn } from "@/lib/utils"
 
+interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {}
+
 const Progress = React.forwardRef
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(function Progress({ className, value, ...props }, ref) {
+  ProgressProps
+>((props, ref) => {
+  const { className, value, ...rest } = props
+  
   return (
     <ProgressPrimitive.Root
       ref={ref}
@@ -15,7 +19,7 @@ const Progress = React.forwardRef
         "relative h-2 w-full overflow-hidden rounded-full bg-gray-200",
         className
       )}
-      {...props}
+      {...rest}
     >
       <ProgressPrimitive.Indicator
         className="h-full w-full flex-1 bg-blue-600 transition-all"
